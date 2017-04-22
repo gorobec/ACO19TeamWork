d857faa7a831567f80d4b661977a1f53c246535d
IvanKnyazev <ivan.iv.knyazev@gmail.com>
2017-04-22 12:17:23 +0300

    add ivan

---
+++ ivan/src/main/java/week1/parking/Car.java
@@ -0,0 +1,11 @@
+package week1.parking;
+
+/**
+ * @author Ivan Knyazev on 19.04.2017
+ * @since JDK 1.8
+ */
+public class Car {
+    public String typeAvto;
+    public String ID;
+    public String nameAvto;
+}
