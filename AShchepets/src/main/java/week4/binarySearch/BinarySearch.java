40e240014020f4ebc87246de0723942ae8b575a6
Yevhen Vorobiei <yvorobiei@ancestry.com>
2017-05-13 00:39:07 +0300

    refactor code

--- AShchepets/src/main/java/week4/binarySearch/BinarySearch.java
+++ AShchepets/src/main/java/week4/binarySearch/BinarySearch.java
@@ -8,48 +8,28 @@ import java.util.Comparator;
  */
 public class BinarySearch {
 
-    public static<T extends Comparable> boolean contains(Comparable<T>[] array, Comparable<T> o) {
+    public static <T extends Comparable<? super T>> boolean contains(T[] array, T o) {
+
+        return contains(array, o, null);
+    }
+
+    public static <T extends Comparable<? super T>> boolean contains(T[] array, T o, Comparator<T> comparator) {
+
+        if(o == null) throw new NullPointerException("Method not support null search: o == null");
 
         int start = 0;
         int end = array.length - 1;
         int middle;
 
-        Arrays.sort(array);
+        Arrays.sort(array, comparator);
 
         while (end >= start) {
 
             middle = (start + end) / 2;
 
-            // без кастирования в Т выдавало ошибку компиляции
-            // Это из-за того,что метод Array.sort() работает с входящими данными
-            // как с объектами и кастирует их внутри своего метода.
-            // Приходится обратно кастировать с объекта в Т после сортировки
-
-            int result = array[middle].compareTo((T) o);
-            if (result == 0) {
-                return true;
-            } else if (result < 0) {
-                start = middle + 1;
-            } else {
-                end = middle - 1;
-            }
-        }
-        return false;
-    }
-
-    public static<T extends Comparator> boolean contains(T[] array, T o, Comparator<T> comparator ) {
-
-        int start = 0;
-        int end = array.length - 1;
-        int middle;
-
-        Arrays.sort(array,comparator);
-
-        while (end>=start) {
-
-            middle = (start + end) / 2;
-
-            int result = comparator.compare(array[middle],o);
+            int result = comparator != null ?
+                    comparator.compare(array[middle], o) :
+                    o.compareTo(array[middle]);
 
             if (result == 0) {
                 return true;
